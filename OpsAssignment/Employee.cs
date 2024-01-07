ce5acfd96fadc39c5e0bd8dbfbd7c846bf9a9491
Pkamgon <105408332+Pkamgon@users.noreply.github.com>
2024-01-07 09:41:14 -0700

    Update Employee.cs

--- OpsAssignment/Employee.cs
+++ OpsAssignment/Employee.cs
@@ -49,6 +49,16 @@ namespace OpsAssignment
         {
             return !(employee1 == employee2);
         }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+
+        }
      
      }
    
