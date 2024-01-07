6dd462cb2de4ad8b17878106eb64b0d5b60c2856
Pkamgon <Pkamgon@gmail.com>
2024-01-07 09:29:59 -0700

    Add project files.

---
+++ OpsAssignment/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Deployment.Internal;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpsAssignment
+{
+    public class Employee
+    {
+        //Properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Employee(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+            //Overload the "==" operator to compare Employees based on Id
+
+            public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (ReferenceEquals(employee1, null) && ReferenceEquals(employee2, null))
+            {
+                return true;
+            }
+
+            // check if either side is null
+            if (ReferenceEquals(employee1, null) && ReferenceEquals(employee2, null))
+
+            {
+                return false;
+            }
+
+            //Compare the Id properties for equality
+            return employee1.Id == employee2.Id;
+
+        }
+
+
+        //Over the "!=" operator using the logic from the "==" operator
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return !(employee1 == employee2);
+        }
+     
+     }
+   
+    
+}
+
+
+
---
+++ OpsAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpsAssignment
+{
+    class Program
+    {
+        //Instantiate two Employee objects
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(1, "Peter", "Paul");
+            Employee employee2 = new Employee(2, "John", "Jones");
+            Employee employee3 = new Employee(1, "Peter", "Paul");
+            
+            //Compare employees using the overloaded operators
+            bool areEqual1 = employee1.Equals(employee2);
+            bool areEqual2 = employee2.Equals(employee3);
+
+            //Display the results
+
+            Console.WriteLine($"Are employee1 and employee2 equal? {areEqual1}"); //should be false
+            Console.WriteLine($"Are employee1 and employee3 equal? {areEqual2}"); // should be true
+        
+        }
+
+
+    }
+}
